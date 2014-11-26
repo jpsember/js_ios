f478c4fabdc674a3e4b0a1a175d483ce41436cf3
Jeff Sember <jpsember@gmail.com>
2014-11-25 18:23:40 -0800

    Issue #27: removed unnecessary 'do ... while false' occurrences

--- js_ios/JSBase.h
+++ js_ios/JSBase.h
@@ -35,41 +35,33 @@ typedef unsigned char byte;
     dispatch_once(&onceToken, __a__ ); \
 }
 
-#define die(__a__,...) do {[JSBase dieWithFilename:__FILE__ line:__LINE__ format:__a__,##__VA_ARGS__];} while (false)
+#define die(__a__,...) [JSBase dieWithFilename:__FILE__ line:__LINE__ format:__a__,##__VA_ARGS__]
 
 #if DEBUG
 
 #define timeStamp(fmt,...) [JSBase showTimeStamp:fmt,##__VA_ARGS__]
-
 // Convert pointer to symbolic string that is easier to read
 #define dp(__id__) [JSBase symbolicNameForId:__id__]
 #define dptr(__ptr__) [JSBase symbolicNameForPtr:__ptr__]
-
 #define __FILE_AND_LINE__ [JSBase descriptionForPath:[NSString stringWithUTF8String:__FILE__] lineNumber:__LINE__]
-
 // For overloading the global variable _DEBUG_PRINTING_
 #pragma GCC diagnostic ignored "-Wshadow"
-
 extern bool _DEBUG_PRINTING_;
 #define DBG const bool _DEBUG_PRINTING_ = true; (void)_DEBUG_PRINTING_;
 #define DBGWARN DBG warning(@"debug printing enabled");
-#define pr(...) {if (_DEBUG_PRINTING_) [JSBase log:__VA_ARGS__];}
-
-#define warning(__a__,...)  { NSString *__s__ = [NSString stringWithFormat:__a__,##__VA_ARGS__]; \
-[JSBase oneTimeReport:__FILE_AND_LINE__ message:__s__ reportType:@"warning      "]; }
-
-#define unimp(__a__,...)  { NSString *__s__ = [NSString stringWithFormat:__a__,##__VA_ARGS__]; \
-[JSBase oneTimeReport:__FILE_AND_LINE__ message:__s__ reportType:@"unimplemented"]; }
-
+#define pr(...) do {if (_DEBUG_PRINTING_) [JSBase log:__VA_ARGS__];} while (false)
+#define warning(__a__,...) [JSBase oneTimeReport:__FILE_AND_LINE__ \
+  message:[NSString stringWithFormat:__a__,##__VA_ARGS__] reportType:@"warning      "]
+#define unimp(__a__,...) [JSBase oneTimeReport:__FILE_AND_LINE__ \
+  message:[NSString stringWithFormat:__a__,##__VA_ARGS__] reportType:@"unimplemented"]
 #define ASSERT(__flag__,__a__,...) do { if (!(__flag__)) die(__a__,##__VA_ARGS__); } while (false)
-
 #define NO_DEFAULT_INITIALIZER -(id)init { die(@"Please call the designated initializer"); return nil; }
 #define USED_DEBUG_ONLY
 
 #else // !DEBUG follows:
 
 #define NO_DEFAULT_INITIALIZER
-#define pr(...) do {} while (false)
+#define pr(...)
 #define ASSERT(__flag__,__a__,...)
 #define warning(__a__,...)
 #define unimp(__a__,...)
